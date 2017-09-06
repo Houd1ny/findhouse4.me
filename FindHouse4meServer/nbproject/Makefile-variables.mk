efd72ec0621cfb9e13abe063bec31cc7b3ed07dd
YuriyP <priymayuriy@gmail.com>
2017-09-06 11:06:53 +0300

    Added server proj

---
+++ FindHouse4meServer/nbproject/Makefile-variables.mk
@@ -0,0 +1,35 @@
+#
+# Generated - do not edit!
+#
+# NOCDDL
+#
+CND_BASEDIR=`pwd`
+CND_BUILDDIR=build
+CND_DISTDIR=dist
+# Debug configuration
+CND_PLATFORM_Debug=GNU-Linux
+CND_ARTIFACT_DIR_Debug=dist/Debug/GNU-Linux
+CND_ARTIFACT_NAME_Debug=findhouse4meserver
+CND_ARTIFACT_PATH_Debug=dist/Debug/GNU-Linux/findhouse4meserver
+CND_PACKAGE_DIR_Debug=dist/Debug/GNU-Linux/package
+CND_PACKAGE_NAME_Debug=findhouse4meserver.tar
+CND_PACKAGE_PATH_Debug=dist/Debug/GNU-Linux/package/findhouse4meserver.tar
+# Release configuration
+CND_PLATFORM_Release=GNU-Linux
+CND_ARTIFACT_DIR_Release=dist/Release/GNU-Linux
+CND_ARTIFACT_NAME_Release=findhouse4meserver
+CND_ARTIFACT_PATH_Release=dist/Release/GNU-Linux/findhouse4meserver
+CND_PACKAGE_DIR_Release=dist/Release/GNU-Linux/package
+CND_PACKAGE_NAME_Release=findhouse4meserver.tar
+CND_PACKAGE_PATH_Release=dist/Release/GNU-Linux/package/findhouse4meserver.tar
+#
+# include compiler specific variables
+#
+# dmake command
+ROOT:sh = test -f nbproject/private/Makefile-variables.mk || \
+	(mkdir -p nbproject/private && touch nbproject/private/Makefile-variables.mk)
+#
+# gmake command
+.PHONY: $(shell test -f nbproject/private/Makefile-variables.mk || (mkdir -p nbproject/private && touch nbproject/private/Makefile-variables.mk))
+#
+include nbproject/private/Makefile-variables.mk
